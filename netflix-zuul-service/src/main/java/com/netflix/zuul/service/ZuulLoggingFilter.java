48d1a346f7abfe43d9858386929cb4f620ef5d83
Khartaz <tjaygallery@gmail.com>
2019-12-06 16:39:14 +0100

    zuul config v1

---
+++ netflix-zuul-service/src/main/java/com/netflix/zuul/service/ZuulLoggingFilter.java
@@ -0,0 +1,39 @@
+package com.netflix.zuul.service;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ZuulLoggingFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+
+        LOGGER.info("request -> {} request uri -> {}", request, request.getRequestURI());
+
+        return null;
+    }
+}
---
+++ netflix-zuul-service/src/test/java/com/netflix/zuul/service/NetflixZuulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netflix.zuul.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixZuulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
