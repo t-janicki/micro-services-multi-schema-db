a8177eae28608259cf80a61ac8e2b9404b1c95a6
Khartaz <tjaygallery@gmail.com>
2019-12-06 17:47:00 +0100

    sleuth config

--- product-service/src/main/java/com/ProductServiceApplication.java
+++ product-service/src/main/java/com/ProductServiceApplication.java
@@ -1,8 +1,10 @@
 package com;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +12,11 @@ public class ProductServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 
 }
